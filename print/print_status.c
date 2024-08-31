35c37053078044ff081b746cd2da3f7546ed0b2f
Minsoo Song <msong@c2r7s8.42seoul.kr>
2024-08-31 20:17:57 +0900

    almost done

--- print/print_status.c
+++
@@ -1,8 +0,0 @@
-#include "../philo.h"
-
-//arguments:current_time, index of philosopher, action
-
-void print_status()
-{
-
-}
--- utility/find_left_fork.c
+++
@@ -1,9 +0,0 @@
-#include "../philo.h"
-
-int find_left_fork(int index, t_data *info)
-{
-    if (index == info->p_num - 1)
-        return (0);
-    else
-        return (index + 1);
-}
